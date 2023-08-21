4a088e02f30c0d3b61a37426562d8367e7f894a0
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-08-21 22:09:30 +0300

    fix

--- reverseLinkedList/reverseList.js
+++ reverseLinkedList/reverseList.js
@@ -1,16 +1,20 @@
 function reverseList(list) {
-  let currentNodes = [list, list.nextList];
+  let currentNodes = [list, list.next];
+  let nextNode = currentNodes[0].next;
+  list.next = null;
 
   while (currentNodes[1]) {
-    let nextNode = currentNodes[1].next;
+    nextNode = currentNodes[1].next;
 
-    currentNodes[0].next = currentNodes[1];
+    currentNodes[1].next = currentNodes[0];
+
+    currentNodes[0] = currentNodes[1];
     currentNodes[1] = nextNode;
   }
 
   return currentNodes[0];
 }
 
-const mockList = {value: 1, next: {value: 2, next: {value: 3, next: {value: 4, next: null}}}};
+const mockList = {value: 1, next: {value: 2, next: {value: 3, next: null}}};
 
 console.log(reverseList(mockList));
