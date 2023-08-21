0b9d88610646d62aefa5085e33a900fa63f15cb7
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-08-21 21:13:15 +0300

    prime Number

---
+++ primeNumber/primeNumber.js
@@ -0,0 +1,30 @@
+function primeNumber(limit) {
+  function isPrime(number) {
+    if (number === false) { return false; }
+
+    for (let del = 2; del <= Math.sqrt(number); del++) {
+      if (number % del !== 0) return false;
+    }
+    return true;
+  }
+
+  const array = [];
+
+  for (let index = 2; index <= limit; index++) {
+    array.push(index);
+  }
+
+  for (let index = 0; index < limit - 1; index++) {
+    const number = array[index];
+
+    if (isPrime(number)) {
+      let index2 = index + number;
+
+      while (index2 < limit - 1) { array[index2] = false; index2 += number; }
+    }
+  }
+
+  return array.filter((item) => item);
+}
+
+console.log(primeNumber(11));
