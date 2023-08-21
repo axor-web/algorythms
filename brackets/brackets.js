44aa729a5d5995000af28f75d876b5db0c836f0f
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-08-21 20:51:08 +0300

    brackets

---
+++ brackets/brackets.js
@@ -0,0 +1,23 @@
+function isBracketsValid(bracketsString) {
+  const stack = [];
+  for (let char of bracketsString) {
+    if ('([{'.includes(char)) {
+      stack.push(char);
+    }
+    else {
+      const lastOpenBracket = stack.pop();
+
+      if (lastOpenBracket === '(' && char !== ')') { return false; }
+      if (lastOpenBracket === '[' && char !== ']') { return false; }
+      if (lastOpenBracket === '{' && char !== '}') { return false; }
+    }
+  }
+
+  return true;
+}
+
+const mockBrackets = ['((((]]]]', '[({})]()', '[((]))'];
+
+for (let brackets of mockBrackets) {
+  console.log(brackets, isBracketsValid(brackets));
+}
